ed46a450523915d7f786cbec5a461cfa71491a55
David Sabeti and Raina Masand <dsabeti@pivotal.io>
2015-03-24 13:48:37 -0700

    Use deployments-core-services instead of deployments-aws

--- v1-dummy-broker/deploy_latest_release.sh
+++ v1-dummy-broker/deploy_latest_release.sh
@@ -11,9 +11,9 @@ source /usr/local/share/chruby/chruby.sh
 chruby 2.1.4
 
 
-source /workspace/deployments-aws/${RELENG_ENV}/bosh_environment
+source /workspace/deployments-core-services/${RELENG_ENV}/bosh_environment
 bosh -n target ${BOSH_PREFIX}.${RELENG_ENV}.cf-app.com
-./generate_deployment_manifest aws /workspace/deployments-aws/$RELENG_ENV/cf-aws-stub.yml /workspace/deployments-aws/$RELENG_ENV/cf-shared-secrets.yml > deployment.yml
+./generate_deployment_manifest aws /workspace/deployments-core-services/$RELENG_ENV/cf-aws-stub.yml /workspace/deployments-core-services/$RELENG_ENV/cf-shared-secrets.yml > deployment.yml
 bosh deployment deployment.yml
 
 bosh create release --force --name $RELEASE_NAME
