a9dc7caa7e49e8d67b9ff9f08321aaf93b27c69f
David Sabeti and Raina Masand <rmasand@pivotal.io>
2015-03-24 10:56:39 -0700

    Fix working directory for delete_deployment

--- upgrade-pipeline/delete_deployment.sh
+++ upgrade-pipeline/delete_deployment.sh
@@ -7,18 +7,18 @@ PRODUCT=$1
 DEPLOYMENT_NAME=cf
 RELEASE_NAME=cf
 
-cd /workspace/cf-release
+cd /workspace
 
 source /usr/local/share/chruby/chruby.sh
 chruby 2.1.4
 
 # If there is a file with the api address, then we must be targetting
 # a transient bosh-lite instance. Otherwise, we're targetting AWS.
-if [ -f ../api-address ]; then
+if [ -f api-address ]; then
   export BOSH_USER=admin
   export BOSH_PASSWORD=admin
   export BOSH_RELEASES_DIR=/workspace
-  export BOSH_LITE_IP=$(cat ../api-address)
+  export BOSH_LITE_IP=$(cat api-address)
   export DEPLOYMENT_NAME=cf
   bosh -n target ${BOSH_LITE_IP}
 
