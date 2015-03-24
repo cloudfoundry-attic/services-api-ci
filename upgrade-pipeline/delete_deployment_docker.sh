578531e1be1b2cf8bebebe24cf74a50831c5ba7c
Jeff Hui <jhui@pivotal.io>
2015-03-23 17:56:22 -0700

    Updated relative path for docker container.

--- upgrade-pipeline/delete_deployment_docker.sh
+++ upgrade-pipeline/delete_deployment_docker.sh
@@ -9,6 +9,6 @@ docker run -a stdout -a stderr -w /workspace/services-api-ci \
     -v $PWD:/workspace\
     -e DEPLOYMENTS_REPO_NAME \
     $DOCKER_IMAGE \
-    /bin/bash -l services-api-ci/upgrade-pipeline/delete_deployment.sh
+    /bin/bash -l upgrade-pipeline/delete_deployment.sh
 
 exit 0
