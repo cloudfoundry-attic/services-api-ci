6db247c48d29d7e0f5838f8a90a7a4f6e264181f
David Sabeti and Greg Cobb <gcobb@pivotal.io>
2015-04-15 15:23:54 -0700

    Add cf-properties.yml to manifest generation for v1-dummy-broker

--- v1-dummy-broker/deploy_latest_release.sh
+++ v1-dummy-broker/deploy_latest_release.sh
@@ -13,7 +13,7 @@ chruby 2.1.4
 
 source /workspace/deployments-core-services/${RELENG_ENV}/bosh_environment
 bosh -n target ${BOSH_PREFIX}.${RELENG_ENV}.cf-app.com
-./generate_deployment_manifest aws /workspace/deployments-core-services/$RELENG_ENV/cf-aws-stub.yml /workspace/deployments-core-services/$RELENG_ENV/cf-shared-secrets.yml /workspace/deployments-core-services/$RELENG_ENV/v1-dummy-broker-secrets.yml > deployment.yml
+./generate_deployment_manifest aws /workspace/deployments-core-services/$RELENG_ENV/cf-aws-stub.yml /workspace/deployments-core-services/$RELENG_ENV/cf-shared-secrets.yml /workspace/deployments-core-services/$RELENG_ENV/v1-dummy-broker-secrets.yml /workspace/deployments-core-services/$RELENG_ENV/cf-properties.yml > deployment.yml
 bosh deployment deployment.yml
 
 bosh create release --force --name $RELEASE_NAME
