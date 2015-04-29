53f30ae1011c32ddfb49be417cd69f047fd7848a
Raina Masand <rmasand@pivotal.io>
2015-04-29 15:13:18 -0700

    Fixing manifest generation for v1-dummy-broker
    - Because a new stub was recently created, and we weren't yet referring
    to it.

--- v1-dummy-broker/promote_latest_release.sh
+++ v1-dummy-broker/promote_latest_release.sh
@@ -13,7 +13,13 @@ chruby 2.1.4
 
 source /workspace/deployments-runtime/${RELENG_ENV}/bosh_environment
 bosh -n target ${BOSH_PREFIX}.${RELENG_ENV}.cf-app.com
-./generate_deployment_manifest aws /workspace/deployments-runtime/$RELENG_ENV/cf-aws-stub.yml /workspace/deployments-runtime/$RELENG_ENV/cf-shared-secrets.yml /workspace/deployments-core-services/$RELENG_ENV/v1-dummy-broker-secrets.yml /workspace/deployments-core-services/$RELENG_ENV/cf-properties.yml > deployment.yml
+./generate_deployment_manifest aws \
+  /workspace/deployments-runtime/$RELENG_ENV/cf-aws-stub.yml \
+  /workspace/deployments-runtime/$RELENG_ENV/cf-shared-secrets.yml \
+  /workspace/deployments-core-services/$RELENG_ENV/v1-dummy-broker-secrets.yml \
+  /workspace/deployments-core-services/$RELENG_ENV/cf-properties.yml \
+  /workspace/deployments-core-services/$RELENG_ENV/cf-networks-stub.yml \
+  > deployment.yml
 bosh deployment deployment.yml
 
 bosh create release --force --name $RELEASE_NAME
