f6c65cbc5004dee0e0d8d3bd342cc5dbcba2d67e
Alex Ley and Greg Cobb <aley@pivotal.io>
2015-05-05 14:37:21 -0700

    Clean up compiled code from workspace
    - GoCD does not create a clean environment for each run, so bumping the
      golang version breaks the pipeline if not cleaned
    
    [#91975604]

--- upgrade-pipeline/add_services.sh
+++ upgrade-pipeline/add_services.sh
@@ -4,8 +4,11 @@ set -xe
 
 GOPATH=/workspace/services-api-ci/upgrade-pipeline
 WORKSPACE_DIR="$(cd $(dirname ${BASH_SOURCE[0]})/../../ && pwd)"
+GODEP_WORKSPACE=$GOPATH/src/github.com/cloudfoundry/cf-acceptance-tests/Godeps/_workspace/
 RELENG_ENV=${RELENG_ENV:-wasabi}
 APPS_DOMAIN=${RELENG_ENV}-app
+
+rm -rf $GODEP_WORKSPACE/pkg
 
 if [ "$RELENG_ENV" == "bosh-lite" ]
 then
@@ -32,7 +35,6 @@ cat > integration_config.json <<EOF
   "skip_ssl_validation": true
 }
 EOF
-
-GOPATH=$GOPATH:$GOPATH/src/github.com/cloudfoundry/cf-acceptance-tests/Godeps/_workspace/ \
+GOPATH=$GOPATH:$GODEP_WORKSPACE \
 CONFIG=`pwd`/integration_config.json \
 ginkgo before_upgrade/
