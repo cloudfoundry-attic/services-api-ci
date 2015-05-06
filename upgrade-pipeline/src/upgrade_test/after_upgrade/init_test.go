ef00f62a07f9211f1203363cac6f647ba9e6080f
Greg Cobb and Raina Masand <gcobb@pivotal.io>
2015-05-06 13:50:49 -0700

    Poll to target cf in after_upgrade tests
    [#91975604]

--- upgrade-pipeline/src/upgrade_test/after_upgrade/init_test.go
+++ upgrade-pipeline/src/upgrade_test/after_upgrade/init_test.go
@@ -6,17 +6,28 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
 
+	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/helpers"
 )
 
 var (
-	DEFAULT_TIMEOUT      = 30 * time.Second
-	CF_PUSH_TIMEOUT      = 2 * time.Minute
-	BROKER_START_TIMEOUT = 5 * time.Minute
+	DEFAULT_TIMEOUT       = 30 * time.Second
+	CF_PUSH_TIMEOUT       = 2 * time.Minute
+	BROKER_START_TIMEOUT  = 5 * time.Minute
+	CF_BOOT_TIMEOUT       = 5 * time.Minute
+	CF_BOOT_POLL_INTERVAL = 1 * time.Second
 )
 
 var context helpers.SuiteContext
+
+var _ = BeforeSuite(func() {
+	apiUrl := context.AdminUserContext().ApiUrl
+	Eventually(func() *Session {
+		return cf.Cf("api", apiUrl, "--skip-ssl-validation").Wait(DEFAULT_TIMEOUT)
+	}, CF_BOOT_TIMEOUT, CF_BOOT_POLL_INTERVAL).Should(Exit(0))
+})
 
 func TestApplications(t *testing.T) {
 	RegisterFailHandler(Fail)
